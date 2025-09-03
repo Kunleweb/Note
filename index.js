53597d43df3adbc79e47209b7bd6b9e1e2c9a2c6
unknown <aadeye302@caledonian.ac.uk>
2025-09-03 23:29:38 +0100

    static

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const uniroute = require('./routes/route')
+const path = require('path')
 
 
 
 
 app.use(express.json())
+
+
+app.use(express.static(path.join(__dirname, 'views')))
+
+
 
 app.use('/api', uniroute)
 
